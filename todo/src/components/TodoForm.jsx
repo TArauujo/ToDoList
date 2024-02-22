76a9950115559c4fdd7e2f7da6f188ae69d599cd
Thomas Carvalho <thomascaraujo19@gmail.com>
2024-02-22 17:06:20 -0300

    Formulário para nova inserção de dados

--- todo/src/components/TodoForm.jsx
+++ todo/src/components/TodoForm.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const TodoForm = () => {
   return (
-    <div>TodoForm</div>
+    <div>
+        <h2>Criar Tarefa:</h2>
+        <form>
+            <input type="text" placeholder="Digite o título"/>
+            <select>
+                <option value="">Selecione uma categoria</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Pessoal">Pessoal</option>
+                <option value="Estudos">Estudos</option>
+            </select>
+            <button type = "submit">Criar Tarefa</button>
+        </form>
+    </div>
   )
 }
 

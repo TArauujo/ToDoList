3d28fb064f7b748d69958e2424115d0ef1be6f60
Thomas Carvalho <thomascaraujo19@gmail.com>
2024-03-04 23:24:43 -0300

    Lógica da função "sort"

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -70,7 +70,10 @@ function App() {
       <div className="todo-list">
         {todos
           .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
-          .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (
+          .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())
+          .sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text))
+          ).map((todo) => (
+          
           <Todo key = {todo.id} todo = {todo} removeTodo = {removeTodo} completeTodo = {completeTodo}/>
         ))}
       </div>

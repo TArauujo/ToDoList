284aaed93b703e0c19e9f47ed28b34835db3ee21
Thomas Carvalho <thomascaraujo19@gmail.com>
2024-03-04 23:21:34 -0300

    Criando a ordenação alfabética

--- todo/src/components/Filter.jsx
+++ todo/src/components/Filter.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Filter = ( {filter, setFilter} ) => {
+const Filter = ( {filter, setFilter, setSort} ) => {
   return (
     <div className="filter">
       <h2>Filtrar:</h2>
@@ -16,8 +16,8 @@ const Filter = ( {filter, setFilter} ) => {
       </div>
       <div>
         <p>Ordem alfabética: </p>
-        <button>Asc</button>
-        <button>Desc</button>
+        <button onClick = {() => setSort("Asc")}>Asc</button>
+        <button onClick = {() => setSort("Desc")}>Desc</button>
       </div>
       </div>
     </div>
